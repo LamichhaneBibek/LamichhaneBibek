08f8f52b5f6986f2b5a514a113be6fc0664a8f84
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2025-12-17 13:33:37 +0545

    update index js

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ const MUSTACHE_MAIN_DIR = './main.mustache';
 const badgeGrid = [
   // Row 1: Programming Languages (First Cell)
   [
-    { alt: 'Go', src: 'https://img.shields.io/badge/-Go-00ADD8?style=flat-square&logo=go&logoColor=white' },
+    { alt: 'Go', src: 'https://img.shields.io/badge/-Go-00ADD8?style=for-the-badge&logo=go&logoColor=white' },
     {
       alt: 'Python',
-      src: 'https://img.shields.io/badge/-Python-3776AB?style=flat-square&logo=python&logoColor=white',
+      src: 'https://img.shields.io/badge/-Python-3776AB?style=for-the-badge&logo=python&logoColor=white',
     },
     {
       alt: 'TypeScript',
-      src: 'https://img.shields.io/badge/-TypeScript-3178C6?style=flat-square&logo=typescript&logoColor=white',
+      src: 'https://img.shields.io/badge/-TypeScript-3178C6?style=for-the-badge&logo=typescript&logoColor=white',
     },
     {
       alt: 'JavaScript',
-      src: 'https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black',
+      src: 'https://img.shields.io/badge/-JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black',
     },
     {
       alt: 'HTML5',
-      src: 'https://img.shields.io/badge/-HTML5-E34F26?style=flat-square&logo=html5&logoColor=white',
+      src: 'https://img.shields.io/badge/-HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white',
     },
     {
       alt: 'CSS3',
-      src: 'https://img.shields.io/badge/-CSS3-1572B6?style=flat-square&logo=css3&logoColor=white',
+      src: 'https://img.shields.io/badge/-CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white',
     },
   ],
 
@@ -37,19 +37,19 @@ const badgeGrid = [
   [
     {
       alt: 'Git',
-      src: 'https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=git&logoColor=white',
+      src: 'https://img.shields.io/badge/-Git-F05032?style=for-the-badge&logo=git&logoColor=white',
     },
     {
       alt: 'GitHub',
-      src: 'https://img.shields.io/badge/-GitHub-181717?style=flat-square&logo=github&logoColor=white',
+      src: 'https://img.shields.io/badge/-GitHub-181717?style=for-the-badge&logo=github&logoColor=white',
     },
     {
       alt: 'Docker',
-      src: 'https://img.shields.io/badge/-Docker-2496ED?style=flat-square&logo=docker&logoColor=white',
+      src: 'https://img.shields.io/badge/-Docker-2496ED?style=for-the-badge&logo=docker&logoColor=white',
     },
     {
       alt: 'Swagger',
-      src: 'https://img.shields.io/badge/-Swagger-85EA2D?style=flat-square&logo=swagger&logoColor=black',
+      src: 'https://img.shields.io/badge/-Swagger-85EA2D?style=for-the-badge&logo=swagger&logoColor=black',
     },
   ],
 
@@ -57,19 +57,19 @@ const badgeGrid = [
   [
     {
       alt: 'FastAPI',
-      src: 'https://img.shields.io/badge/-FastAPI-009688?style=flat-square&logo=fastapi&logoColor=white',
+      src: 'https://img.shields.io/badge/-FastAPI-009688?style=for-the-badge&logo=fastapi&logoColor=white',
     },
     {
       alt: 'Node.js',
-      src: 'https://img.shields.io/badge/-Node.js-339933?style=flat-square&logo=node.js&logoColor=white',
+      src: 'https://img.shields.io/badge/-Node.js-339933?style=for-the-badge&logo=node.js&logoColor=white',
     },
     {
       alt: 'GraphQL',
-      src: 'https://img.shields.io/badge/-GraphQL-E10098?style=flat-square&logo=graphql&logoColor=white',
+      src: 'https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&logo=graphql&logoColor=white',
     },
     {
       alt: 'REST API',
-      src: 'https://img.shields.io/badge/-REST_API-FF6C37?style=flat-square&logo=postman&logoColor=white',
+      src: 'https://img.shields.io/badge/-REST_API-FF6C37?style=for-the-badge&logo=postman&logoColor=white',
     },
   ],
 
@@ -77,19 +77,19 @@ const badgeGrid = [
   [
     {
       alt: 'React',
-      src: 'https://img.shields.io/badge/-React-61DAFB?style=flat-square&logo=react&logoColor=black',
+      src: 'https://img.shields.io/badge/-React-61DAFB?style=for-the-badge&logo=react&logoColor=black',
     },
     {
       alt: 'Next.js',
-      src: 'https://img.shields.io/badge/-Next.js-000000?style=flat-square&logo=next.js&logoColor=white',
+      src: 'https://img.shields.io/badge/-Next.js-000000?style=for-the-badge&logo=next.js&logoColor=white',
     },
     {
       alt: 'D3.js',
-      src: 'https://img.shields.io/badge/-D3.js-F9A03C?style=flat-square&logo=d3.js&logoColor=white',
+      src: 'https://img.shields.io/badge/-D3.js-F9A03C?style=for-the-badge&logo=d3.js&logoColor=white',
     },
     {
       alt: 'NPM',
-      src: 'https://img.shields.io/badge/-NPM-CB3837?style=flat-square&logo=npm&logoColor=white',
+      src: 'https://img.shields.io/badge/-NPM-CB3837?style=for-the-badge&logo=npm&logoColor=white',
     },
   ],
 
@@ -97,19 +97,19 @@ const badgeGrid = [
   [
     {
       alt: 'PostgreSQL',
-      src: 'https://img.shields.io/badge/-PostgreSQL-4169E1?style=flat-square&logo=postgresql&logoColor=white',
+      src: 'https://img.shields.io/badge/-PostgreSQL-4169E1?style=for-the-badge&logo=postgresql&logoColor=white',
     },
     {
       alt: 'MongoDB',
-      src: 'https://img.shields.io/badge/-MongoDB-47A248?style=flat-square&logo=mongodb&logoColor=white',
+      src: 'https://img.shields.io/badge/-MongoDB-47A248?style=for-the-badge&logo=mongodb&logoColor=white',
     },
     {
       alt: 'MySQL',
-      src: 'https://img.shields.io/badge/-MySQL-4479A1?style=flat-square&logo=mysql&logoColor=white',
+      src: 'https://img.shields.io/badge/-MySQL-4479A1?style=for-the-badge&logo=mysql&logoColor=white',
     },
     {
       alt: 'SQLite',
-      src: 'https://img.shields.io/badge/-SQLite-003B57?style=flat-square&logo=sqlite&logoColor=white',
+      src: 'https://img.shields.io/badge/-SQLite-003B57?style=for-the-badge&logo=sqlite&logoColor=white',
     },
   ],
 
@@ -117,15 +117,15 @@ const badgeGrid = [
   [
     {
       alt: 'VSCode',
-      src: 'https://img.shields.io/badge/-VSCode-007ACC?style=flat-square&logo=visual-studio-code&logoColor=white',
+      src: 'https://img.shields.io/badge/-VSCode-007ACC?style=for-the-badge&logo=visual-studio-code&logoColor=white',
     },
     {
       alt: 'NeoVim',
-      src: 'https://img.shields.io/badge/-NeoVim-57A143?style=flat-square&logo=neovim&logoColor=white',
+      src: 'https://img.shields.io/badge/-NeoVim-57A143?style=for-the-badge&logo=neovim&logoColor=white',
     },
     {
       alt: 'Zed',
-      src: 'https://img.shields.io/badge/-Zed-084CCF?style=flat-square&logo=zed&logoColor=white',
+      src: 'https://img.shields.io/badge/-Zed-084CCF?style=for-the-badge&logo=zed&logoColor=white',
     },
   ],
 
@@ -133,36 +133,36 @@ const badgeGrid = [
   [
     {
       alt: 'Postman',
-      src: 'https://img.shields.io/badge/-Postman-FF6C37?style=flat-square&logo=postman&logoColor=white',
+      src: 'https://img.shields.io/badge/-Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white',
     },
     {
       alt: 'Insomnia',
-      src: 'https://img.shields.io/badge/-Insomnia-4000BF?style=flat-square&logo=insomnia&logoColor=white',
+      src: 'https://img.shields.io/badge/-Insomnia-4000BF?style=for-the-badge&logo=insomnia&logoColor=white',
     },
     {
       alt: 'cURL',
-      src: 'https://img.shields.io/badge/-cURL-073551?style=flat-square&logo=curl&logoColor=white',
+      src: 'https://img.shields.io/badge/-cURL-073551?style=for-the-badge&logo=curl&logoColor=white',
     },
-    { alt: 'UV', src: 'https://img.shields.io/badge/-UV-DE5FE9?style=flat-square&logo=uv&logoColor=white' },
+    { alt: 'UV', src: 'https://img.shields.io/badge/-UV-DE5FE9?style=for-the-badge&logo=uv&logoColor=white' },
   ],
 
   // Row 8: Cloud & Deployment Platforms
   [
     {
       alt: 'Heroku',
-      src: 'https://img.shields.io/badge/-Heroku-430098?style=flat-square&logo=heroku&logoColor=white',
+      src: 'https://img.shields.io/badge/-Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white',
     },
     {
       alt: 'Vercel',
-      src: 'https://img.shields.io/badge/-Vercel-000000?style=flat-square&logo=vercel&logoColor=white',
+      src: 'https://img.shields.io/badge/-Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white',
     },
     {
       alt: 'Netlify',
-      src: 'https://img.shields.io/badge/-Netlify-00C7B7?style=flat-square&logo=netlify&logoColor=white',
+      src: 'https://img.shields.io/badge/-Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white',
     },
     {
       alt: 'Railway',
-      src: 'https://img.shields.io/badge/-Railway-0B0D0E?style=flat-square&logo=railway&logoColor=white',
+      src: 'https://img.shields.io/badge/-Railway-0B0D0E?style=for-the-badge&logo=railway&logoColor=white',
     },
   ],
 
@@ -170,15 +170,15 @@ const badgeGrid = [
   [
     {
       alt: 'Cloudflare',
-      src: 'https://img.shields.io/badge/-Cloudflare-F38020?style=flat-square&logo=cloudflare&logoColor=white',
+      src: 'https://img.shields.io/badge/-Cloudflare-F38020?style=for-the-badge&logo=cloudflare&logoColor=white',
     },
     {
       alt: 'Render',
-      src: 'https://img.shields.io/badge/-Render-46E3B7?style=flat-square&logo=render&logoColor=white',
+      src: 'https://img.shields.io/badge/-Render-46E3B7?style=for-the-badge&logo=render&logoColor=white',
     },
     {
       alt: 'GitHub Actions',
-      src: 'https://img.shields.io/badge/-GitHub_Actions-2088FF?style=flat-square&logo=github-actions&logoColor=white',
+      src: 'https://img.shields.io/badge/-GitHub_Actions-2088FF?style=for-the-badge&logo=github-actions&logoColor=white',
     },
   ],
 
@@ -186,15 +186,15 @@ const badgeGrid = [
   [
     {
       alt: 'Linux',
-      src: 'https://img.shields.io/badge/-Linux-FCC624?style=flat-square&logo=linux&logoColor=black',
+      src: 'https://img.shields.io/badge/-Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black',
     },
     {
       alt: 'Ubuntu',
-      src: 'https://img.shields.io/badge/-Ubuntu-E95420?style=flat-square&logo=ubuntu&logoColor=white',
+      src: 'https://img.shields.io/badge/-Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white',
     },
     {
       alt: 'Firefox',
-      src: 'https://img.shields.io/badge/-Firefox-FF7139?style=flat-square&logo=firefox&logoColor=white',
+      src: 'https://img.shields.io/badge/-Firefox-FF7139?style=for-the-badge&logo=firefox&logoColor=white',
     },
   ],
 ];
